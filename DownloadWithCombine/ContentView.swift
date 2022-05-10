d7b7d9ff642c671d5ef441fe6ebe4329b6714293
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-11 01:00:15 +0500

    Get request for Combine

--- DownloadWithCombine/ContentView.swift
+++ DownloadWithCombine/ContentView.swift
@@ -16,6 +16,7 @@ struct Posts: Identifiable, Codable {
 
 class DownloadWithCombine: ObservableObject {
     @Published var posts: [Posts] = []
+    var cancelable = Set<AnyCancellable>()
     
     init() {
         getPosts()
@@ -23,8 +24,25 @@ class DownloadWithCombine: ObservableObject {
     
     func getPosts() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
-        
-                
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap { (data, response) -> Data in
+                guard
+                    let response = response as? HTTPURLResponse,
+                    response.statusCode >= 200 && response.statusCode < 300 else {
+                        throw URLError(.badServerResponse)
+                    }
+                return data
+            }
+            .decode(type: [Posts].self, decoder: JSONDecoder())
+            .sink { completion in
+                print("\(completion)")
+            } receiveValue: { [weak self] posts in
+                self?.posts = posts
+            }
+            .store(in: &cancelable)
+
     }
 }
 
