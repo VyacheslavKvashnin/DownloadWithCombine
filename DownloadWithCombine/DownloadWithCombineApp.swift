336a0e5001daf9ddf05f6399533897fafc771914
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-10 23:43:43 +0500

    Initial Commit

---
+++ DownloadWithCombine/DownloadWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithCombineApp.swift
+//  DownloadWithCombine
+//
+//  Created by Вячеслав Квашнин on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
