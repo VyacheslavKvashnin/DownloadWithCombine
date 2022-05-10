6322c8e7450cba7a5a17e520d1c3b77a529e3463
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-11 01:05:17 +0500

    Split into different files

--- DownloadWithCombine/ContentView.swift
+++ DownloadWithCombine/ContentView.swift
@@ -6,48 +6,10 @@
 //
 
 import SwiftUI
-import Combine
-
-struct Posts: Identifiable, Codable {
-    let id: Int
-    let title: String
-    let body: String
-}
-
-class DownloadWithCombine: ObservableObject {
-    @Published var posts: [Posts] = []
-    var cancelable = Set<AnyCancellable>()
-    
-    init() {
-        getPosts()
-    }
-    
-    func getPosts() {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
-        URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .background))
-            .receive(on: DispatchQueue.main)
-            .tryMap { (data, response) -> Data in
-                guard
-                    let response = response as? HTTPURLResponse,
-                    response.statusCode >= 200 && response.statusCode < 300 else {
-                        throw URLError(.badServerResponse)
-                    }
-                return data
-            }
-            .decode(type: [Posts].self, decoder: JSONDecoder())
-            .sink { completion in
-                print("\(completion)")
-            } receiveValue: { [weak self] posts in
-                self?.posts = posts
-            }
-            .store(in: &cancelable)
-
-    }
-}
 
 struct ContentView: View {
-    @StateObject var viewModel = DownloadWithCombine()
+    @StateObject var viewModel = DownloadViewModel()
+    
     var body: some View {
         List {
             ForEach(viewModel.posts) { post in
---
+++ DownloadWithCombine/Posts.swift
@@ -0,0 +1,12 @@
+//
+//  Posts.swift
+//  DownloadWithCombine
+//
+//  Created by Вячеслав Квашнин on 11.05.2022.
+//
+
+struct Posts: Identifiable, Codable {
+    let id: Int
+    let title: String
+    let body: String
+}
